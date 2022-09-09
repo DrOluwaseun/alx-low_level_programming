f0584a55e26ef41a8d606bf1e8d20d18277aad6d
DrOluwaseun <dadajamesoluwaseun1985@gmail.com>
2022-09-08 19:36:56 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main -print the string in the put function
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual
+puzzle");
+	return (0);
+}
