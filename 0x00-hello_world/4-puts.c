74a28397217f43a73385278fabdb0b5bf700f816
DrOluwaseun <dadajamesoluwaseun1985@gmail.com>
2022-09-09 18:43:17 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  *main -print the string in the put function
  *
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
