52fc63cca4dfeb2fb0bbde427fdad07989c6accd
DrOluwaseun <dadajamesoluwaseun1985@gmail.com>
2022-09-09 19:01:01 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,14 @@ vim 5-printf.c
 #include <stdio.h>
 
 /**
- * * main - Prints a text
- * *
- * * Return: Always 0 (Success)
- * */
+ *  main - Prints a text
+ *
+ *  Return: Always 0 (Success)
+ * /
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of
 			art,\n");
 	return (0);
+
+}	
