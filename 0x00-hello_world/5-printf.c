9f0b401ceb9c859bd31d1867f198c3744e7e3634
DrOluwaseun <dadajamesoluwaseun1985@gmail.com>
2022-09-10 03:05:47 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * Description: main- Prints out string 
  * Return: 0 if sucesss.
  */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
-return (0);
+                printf("with proper grammar, but the outcome is a piece
+	       	of art, \n");
+                return (0);
 }
